b31edac875185d7232e59900d1be761a2299d8fd
Darkcodelab <deepachakaravarthi434@gmail.com>
2021-09-20 12:09:18 +0530

    close the menu once menu link is clicked

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".menu");
 const navUl = document.querySelector(".nav-ul");
+const navLinks = document.querySelectorAll(".nav-link");
 
 menu.addEventListener("click", function () {
   navUl.classList.toggle("nav-ul--active");
   menu.classList.toggle("menu--click");
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", function () {
+    navUl.classList.remove("nav-ul--active");
+    menu.classList.remove("menu--click");
+  });
 });
